2b267af39ef56e67b7366899018e29de2e8008b2
Alexander Schulz <schulz.alexander2002@gmail.com>
2025-05-13 10:51:27 +0200

    reader controlling each ring separately

--- reader/reader.ino
+++ reader/reader.ino
@@ -7,25 +7,29 @@
 #define RST_PIN1 9    // First reader RST
 #define SS_PIN2 7     // Second reader SS/SDA
 #define RST_PIN2 8    // Second reader RST
-#define LED_RING_PIN 5  // LED ring data input pin
+#define LED_RING_PIN1 5  // First LED ring data input pin
+#define LED_RING_PIN2 6  // Second LED ring data input pin
 
-// Number of LEDs in the ring
+// Number of LEDs in the rings
 #define NUM_LEDS 12
 
 // Create MFRC522 instances
 MFRC522 rfid1(SS_PIN1, RST_PIN1);
 MFRC522 rfid2(SS_PIN2, RST_PIN2);
 
-// Create LED ring instance
-Adafruit_NeoPixel ring = Adafruit_NeoPixel(NUM_LEDS, LED_RING_PIN, NEO_GRB + NEO_KHZ800);
+// Create LED ring instances
+Adafruit_NeoPixel ring1 = Adafruit_NeoPixel(NUM_LEDS, LED_RING_PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ring2 = Adafruit_NeoPixel(NUM_LEDS, LED_RING_PIN2, NEO_GRB + NEO_KHZ800);
 
 // Define the known tag UIDs
 byte knownTag1[4] = {0xCA, 0x6E, 0xEF, 0x3F}; // First tag - green color
 byte knownTag2[4] = {0x76, 0x43, 0x42, 0x29}; // Second tag - red color
 
 // Variables to track current tag state
-bool tagDetected = false;
-unsigned long lastDetectionTime = 0;
+bool tag1Detected = false;
+bool tag2Detected = false;
+unsigned long lastDetectionTime1 = 0;
+unsigned long lastDetectionTime2 = 0;
 const unsigned long TAG_TIMEOUT = 500; // ms to wait before considering tag gone
 
 void setup() {
@@ -36,31 +40,44 @@ void setup() {
     rfid1.PCD_Init();
     rfid2.PCD_Init();
     
-    // Initialize LED ring
-    ring.begin();
-    ring.show();  // Initialize all pixels to 'off'
-    ring.setBrightness(50);  // Set brightness (0-255)
+    // Initialize LED rings
+    ring1.begin();
+    ring1.show();  // Initialize all pixels to 'off'
+    ring1.setBrightness(50);  // Set brightness (0-255)
+    
+    ring2.begin();
+    ring2.show();  // Initialize all pixels to 'off'
+    ring2.setBrightness(50);  // Set brightness (0-255)
     
     Serial.println("Place your RFID tag near either reader...");
 }
 
 void loop() {
-    bool tag1Present = checkReader(rfid1, "Reader 1");
-    bool tag2Present = checkReader(rfid2, "Reader 2");
+    // Check readers and update corresponding LED rings
+    bool reader1Active = checkReader(rfid1, "Reader 1", 1);
+    bool reader2Active = checkReader(rfid2, "Reader 2", 2);
     
-    // If no tags detected for a while, turn off the ring
-    if (!tag1Present && !tag2Present) {
-        Serial.println("No tag detected");
-        if (tagDetected && (millis() - lastDetectionTime > TAG_TIMEOUT)) {
-
-            tagDetected = false;
-            setRingColor(0, 0, 0); // Turn off
+    // Check timeout for reader 1
+    if (!reader1Active) {
+        if (tag1Detected && (millis() - lastDetectionTime1 > TAG_TIMEOUT)) {
+            Serial.println("Reader 1 - No tag detected");
+            tag1Detected = false;
+            setRingColor(ring1, 0, 0, 0); // Turn off
+        }
+    }
+    
+    // Check timeout for reader 2
+    if (!reader2Active) {
+        if (tag2Detected && (millis() - lastDetectionTime2 > TAG_TIMEOUT)) {
+            Serial.println("Reader 2 - No tag detected");
+            tag2Detected = false;
+            setRingColor(ring2, 0, 0, 0); // Turn off
         }
     }
 }
 
 // Function to check a specific reader
-bool checkReader(MFRC522 &rfid, String readerName) {
+bool checkReader(MFRC522 &rfid, String readerName, int readerNum) {
     // Look for new cards on this reader
     if (!rfid.PICC_IsNewCardPresent()) {
         return false;
@@ -71,9 +88,14 @@ bool checkReader(MFRC522 &rfid, String readerName) {
         return false;
     }
     
-    // Tag detected, update tracking
-    tagDetected = true;
-    lastDetectionTime = millis();
+    // Tag detected, update tracking based on which reader
+    if (readerNum == 1) {
+        tag1Detected = true;
+        lastDetectionTime1 = millis();
+    } else {
+        tag2Detected = true;
+        lastDetectionTime2 = millis();
+    }
     
     // Display UID in Serial Monitor
     Serial.print(readerName + " - RFID Tag UID:");
@@ -83,26 +105,30 @@ bool checkReader(MFRC522 &rfid, String readerName) {
     }
     Serial.println();
     
+    // Select which ring to use based on the reader number
+    Adafruit_NeoPixel& currentRing = (readerNum == 1) ? ring1 : ring2;
+    
     // Check if it's our first tag
     if (compareUID(rfid.uid.uidByte, knownTag1)) {
-        Serial.println("Green tag detected!");
-        setRingColor(0, 255, 0);  // Set ring to green
+        Serial.println(readerName + " - Green tag detected!");
+        setRingColor(currentRing, 0, 255, 0);  // Set ring to green
     }
     // Check if it's our second tag
     else if (compareUID(rfid.uid.uidByte, knownTag2)) {
-        Serial.println("Red tag detected!");
-        setRingColor(255, 0, 0);  // Set ring to red
+        Serial.println(readerName + " - Red tag detected!");
+        setRingColor(currentRing, 255, 0, 0);  // Set ring to red
     }
     else {
-        Serial.println("Unknown tag");
+        Serial.println(readerName + " - Unknown tag");
         // Rainbow effect for unknown tag
         for (int i = 0; i < 3; i++) {
-            rainbowCycle(10);
+            rainbowCycle(currentRing, 10);
         }
-        setRingColor(0, 0, 255);  // Blue for unknown
+        setRingColor(currentRing, 0, 0, 255);  // Blue for unknown
     }
     
-    rfid.PICC_HaltA(); // Halt PICC_HaltA
+    rfid.PICC_HaltA(); // Halt PICC
+    rfid.PCD_StopCrypto1(); // Stop encryption on PCD
     return true;
 }
 
@@ -116,16 +142,16 @@ bool compareUID(byte* uid1, byte* uid2) {
     return true;
 }
 
-// Set all pixels in the ring to the same color
-void setRingColor(uint8_t r, uint8_t g, uint8_t b) {
+// Set all pixels in the specified ring to the same color
+void setRingColor(Adafruit_NeoPixel &ring, uint8_t r, uint8_t g, uint8_t b) {
     for (int i = 0; i < NUM_LEDS; i++) {
         ring.setPixelColor(i, ring.Color(r, g, b));
     }
     ring.show();
 }
 
-// Rainbow cycle effect for unknown tags
-void rainbowCycle(uint8_t wait) {
+// Rainbow cycle effect for unknown tags on the specified ring
+void rainbowCycle(Adafruit_NeoPixel &ring, uint8_t wait) {
     for (int j = 0; j < 256; j++) {
         for (int i = 0; i < NUM_LEDS; i++) {
             ring.setPixelColor(i, wheel(((i * 256 / NUM_LEDS) + j) & 255));
@@ -140,12 +166,12 @@ void rainbowCycle(uint8_t wait) {
 uint32_t wheel(byte wheelPos) {
     wheelPos = 255 - wheelPos;
     if (wheelPos < 85) {
-        return ring.Color(255 - wheelPos * 3, 0, wheelPos * 3);
+        return ring1.Color(255 - wheelPos * 3, 0, wheelPos * 3);
     }
     if (wheelPos < 170) {
         wheelPos -= 85;
-        return ring.Color(0, wheelPos * 3, 255 - wheelPos * 3);
+        return ring1.Color(0, wheelPos * 3, 255 - wheelPos * 3);
     }
     wheelPos -= 170;
-    return ring.Color(wheelPos * 3, 255 - wheelPos * 3, 0);
+    return ring1.Color(wheelPos * 3, 255 - wheelPos * 3, 0);
 }
